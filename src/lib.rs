032d6d2c96257d8ac40fc10464872fda9198f406
Benjamin Sherman <Benjamin.Sherman@leidos.com>
2020-07-22 20:45:48 -0400

    Remove CURR_TIMES

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@ use std::collections::{HashMap, HashSet};
 use wasm_bindgen::prelude::*;
 
 lazy_static! {
-    static ref CURR_TIMES: RwLock<[u64; 3]> = RwLock::new([0; 3]);
     static ref SCHEDULES: RwLock<Vec<Vec<u32>>> = RwLock::new(Vec::new());
     static ref SELECTED_COURSES: RwLock<HashMap<&'static str, HashSet<u32>>> =
         RwLock::new(HashMap::new());
@@ -37,7 +36,6 @@ pub fn generate_schedules_and_conflicts() -> usize {
         console_log!("Called generateSchedulesAndConflicts with no schedules, short circuiting!");
 
         *SCHEDULES.write().unwrap() = Vec::new();
-        *CURR_TIMES.write().unwrap() = [0; 3];
 
         0
     } else {
@@ -45,9 +43,7 @@ pub fn generate_schedules_and_conflicts() -> usize {
 
         bm_start!("generate schedules");
         #[allow(unused_assignments)]
-        let mut times = CURR_TIMES.write().unwrap();
-        *times = [u64::MAX; 3];
-        let schedules = generate_schedules_driver(&mut selected_courses, &mut times);
+        let schedules = generate_schedules_driver(&mut selected_courses, false);
         bm_end!("generate schedules");
 
         let schedules_len = schedules.len();
@@ -58,15 +54,11 @@ pub fn generate_schedules_and_conflicts() -> usize {
     };
 
     console_log!("Generated {} schedules", schedules_len);
-    console_log!("Conflicting times: {:?}", *CURR_TIMES.read().unwrap());
 
     schedules_len
 }
 
-fn generate_schedules_driver(
-    courses: &mut Vec<&HashSet<u32>>,
-    global_times: &mut [u64; 3],
-) -> Vec<Vec<u32>> {
+fn generate_schedules_driver(courses: &mut Vec<&HashSet<u32>>, abort_early: bool) -> Vec<Vec<u32>> {
     courses.sort_by_cached_key(|set| set.len());
 
     let (required_times, conflict) = courses
@@ -90,7 +82,7 @@ fn generate_schedules_driver(
     } else if required_times.iter().all(|cnf| *cnf == 0) {
         // There are no 1 section courses, so we can skip all of the below
         let courses = courses.iter().map(|set| (*set).clone()).collect();
-        return generate_schedules(0, &courses, &mut Vec::new(), &mut [0; 3], global_times);
+        return generate_schedules(0, &courses, &mut Vec::new(), &mut [0; 3], abort_early);
     }
 
     let mut courses: Vec<HashSet<u32>> = courses
@@ -117,7 +109,7 @@ fn generate_schedules_driver(
     if courses[0].len() == 0 {
         Vec::new()
     } else {
-        generate_schedules(0, &courses, &mut Vec::new(), &mut [0; 3], global_times)
+        generate_schedules(0, &courses, &mut Vec::new(), &mut [0; 3], abort_early)
     }
 }
 
@@ -126,10 +118,9 @@ fn generate_schedules(
     courses: &Vec<HashSet<u32>>,
     current_courses: &mut Vec<u32>,
     current_times: &mut [u64; 3],
-    overall_times: &mut [u64; 3],
+    abort_early: bool,
 ) -> Vec<Vec<u32>> {
     if idx >= courses.len() {
-        *overall_times = bitwise_and(overall_times, current_times);
         return vec![current_courses.clone()];
     }
 
@@ -150,8 +141,13 @@ fn generate_schedules(
             courses,
             current_courses,
             current_times,
-            overall_times,
+            abort_early,
         ));
+
+        if abort_early && !ret.is_empty() {
+            // We just care that one schedule was found, so return that schedule
+            return ret;
+        }
 
         current_courses.pop();
         *current_times = bitwise_xor(current_times, curr_sec_times);
@@ -191,20 +187,9 @@ pub fn set_selected(crn: u32, selected: bool) {
     console_log!("Selected courses: {:?}", *selected_courses_map);
 }
 
-#[wasm_bindgen(js_name = "everythingConflicts")]
-pub fn everything_conflicts() -> bool {
-    CURR_TIMES
-        .read()
-        .unwrap()
-        .iter()
-        .all(|cnf| cnf == &u64::MAX)
-}
-
 #[wasm_bindgen(js_name = "isInConflict")]
 pub fn is_in_conflict(crn: u32) -> bool {
-    if everything_conflicts() {
-        return true;
-    } else if !CRN_COURSES.contains_key(&crn) {
+    if !CRN_COURSES.contains_key(&crn) {
         // Old CRN, ignore
         return false;
     }
@@ -212,37 +197,30 @@ pub fn is_in_conflict(crn: u32) -> bool {
     let selected_courses_map = SELECTED_COURSES.read().unwrap();
     let course_name = CRN_COURSES.get(&crn).unwrap();
 
-    if selected_courses_map.get(course_name).is_some() {
+    let mut course_vec: Vec<&HashSet<u32>> = if selected_courses_map.get(course_name).is_some() {
         // Assuming we didn't have other sections from this course selected, do we conflict?
         // This check stops us from saying that every section of a course conflicts if we've
         // just selected one, since courses usually have a common timeslot (e.g. test slots).
         // It also lets users see which sections they selected conflict with stuff.
-        let mut course_vec: Vec<&HashSet<u32>> = selected_courses_map
+        selected_courses_map
             .iter()
             .filter(|(name, _)| name != &course_name)
             .map(|(_, sections)| sections)
-            .collect();
-        let mut tmp_set = HashSet::new();
-        tmp_set.insert(crn);
-        course_vec.push(&tmp_set);
-
-        generate_schedules_driver(&mut course_vec, &mut [0; 3]).is_empty()
+            .collect()
     } else {
-        // This is a normal section, so we can just check if the timeslots overlap
-        let crn_times = CRN_TIMES.get(&crn).unwrap();
-        let curr_times = CURR_TIMES.read().unwrap();
-
-        console_log!(
-            "Checking if crn {} ({:?}) conflicts with global schedule of {:?}",
-            crn,
-            crn_times,
-            *curr_times
-        );
-
-        bitwise_and(crn_times, &curr_times)
+        // Since we don't have any section of this course selected, we don't need to filter
+        // anything out of the course_vec
+        selected_courses_map
             .iter()
-            .any(|conf| conf != &0)
-    }
+            .map(|(_, sections)| sections)
+            .collect()
+    };
+
+    let mut tmp_set = HashSet::new();
+    tmp_set.insert(crn);
+    course_vec.push(&tmp_set);
+
+    generate_schedules_driver(&mut course_vec, true).is_empty()
 }
 
 #[wasm_bindgen(js_name = "getSchedule")]
